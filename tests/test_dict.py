97d3c5e8cc0580eb5f9dddedd9abedf98cdb385c
Brendan Cazier <admin@brendancazier.com>
2020-03-05 13:48:40 -0600

    Added tests for stores.dict
    Former-commit-id: a9d1116a709e9daee39b24aef058a74c19d87695

---
+++ tests/test_dict.py
@@ -0,0 +1,27 @@
+import help_test
+import stores.dict as storage
+
+
+def test_module():
+    assert type(storage.GAMES) == dict
+    assert len(storage.GAMES) == 0
+
+
+def test_push():
+    storage.push(room=u"ABCD", value=u"TESTDATA")
+
+    assert len(storage.GAMES) == 1
+    assert storage.GAMES["ABCD"] == u"TESTDATA"
+
+
+def test_pull():
+    pull = storage.pull(room=u"ABCD")
+
+    assert pull is not None
+
+
+def test_rooms():
+    rooms = storage.rooms()
+
+    assert rooms is not None
+
