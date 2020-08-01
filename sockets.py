64f2263466c5269e863381903fba90eaa3dd92f5
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-08-01 18:28:45 -0500

    Removed some debugging stuff
    Former-commit-id: b9a5c203fbfe3400e6c4073b330974d509c337ab

--- sockets.py
+++ sockets.py
@@ -1,10 +1,4 @@
 from flask_socketio import SocketIO
 
 
-class LogSocketIO(SocketIO):
-    def emit(self, event, *args, **kwargs):
-        print(f"EMITTING WEBSOCKET! ==== {event}\n")
-        super().emit(event, *args, **kwargs)
-
-
-socketio = LogSocketIO()
+socketio = SocketIO()
