9b107ef3c0eee89432160b1d6b5dd3a2dae5883e
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-08-22 12:53:11 -0500

    Indenting

--- api/dbapi/routes.py
+++ api/dbapi/routes.py
@@ -55,57 +55,57 @@ class SetListResource(Resource):
         if set(payload.keys()) == set(
             ("date", "show", "round", "complete", "category", "value", "external", "question", "answer")
         ) and all((len(str(v)) > 0 for k, v in payload.items())):
-        if (date := Date.query.filter_by(date=datetime.date.fromisoformat(payload["date"])).first()) is None:
-            date = Date(date=datetime.date.fromisoformat(payload["date"]))
-            db.session.add(date)
+            if (date := Date.query.filter_by(date=datetime.date.fromisoformat(payload["date"])).first()) is None:
+                date = Date(date=datetime.date.fromisoformat(payload["date"]))
+                db.session.add(date)
 
-        if (show := Show.query.filter_by(number=payload["show"]).first()) is None:
-            show = Show(number=payload["show"], date=date)
-            db.session.add(show)
+            if (show := Show.query.filter_by(number=payload["show"]).first()) is None:
+                show = Show(number=payload["show"], date=date)
+                db.session.add(show)
 
-        if (round_ := Round.query.filter_by(number=payload["round"]).first()) is None:
-            round_ = Round(number=payload["round"])
-            db.session.add(round_)
+            if (round_ := Round.query.filter_by(number=payload["round"]).first()) is None:
+                round_ = Round(number=payload["round"])
+                db.session.add(round_)
 
-        if (complete := Complete.query.filter_by(state=payload["complete"]).first()) is None:
-            complete = Complete(state=payload["complete"])
-            db.session.add(complete)
+            if (complete := Complete.query.filter_by(state=payload["complete"]).first()) is None:
+                complete = Complete(state=payload["complete"])
+                db.session.add(complete)
 
-        if (category := Category.query.filter_by(name=payload["category"]).first()) is None:
-            category = Category(name=payload["category"], show=show, round=round_, complete=complete, date=date)
-            db.session.add(category)
+            if (category := Category.query.filter_by(name=payload["category"]).first()) is None:
+                category = Category(name=payload["category"], show=show, round=round_, complete=complete, date=date)
+                db.session.add(category)
 
-        if (value := Value.query.filter_by(amount=payload["value"]).first()) is None:
-            value = Value(amount=payload["value"], round=round_)
-            db.session.add(value)
+            if (value := Value.query.filter_by(amount=payload["value"]).first()) is None:
+                value = Value(amount=payload["value"], round=round_)
+                db.session.add(value)
 
-        if (external := External.query.filter_by(state=payload["external"]).first()) is None:
-            external = External(state=payload["external"])
-            db.session.add(external)
+            if (external := External.query.filter_by(state=payload["external"]).first()) is None:
+                external = External(state=payload["external"])
+                db.session.add(external)
 
-        hash = zlib.adler32(f'{payload["question"]}{payload["answer"]}{show.number}'.encode())
+            hash = zlib.adler32(f'{payload["question"]}{payload["answer"]}{show.number}'.encode())
 
-        if Set.query.filter_by(hash=hash).first() is None:
-            set_ = Set(
-                category=category,
-                date=date,
-                show=show,
-                round=round_,
-                answer=payload["answer"],
-                question=payload["question"],
-                value=value,
-                external=external,
-                complete=complete,
-                hash=hash,
-            )
+            if Set.query.filter_by(hash=hash).first() is None:
+                set_ = Set(
+                    category=category,
+                    date=date,
+                    show=show,
+                    round=round_,
+                    answer=payload["answer"],
+                    question=payload["question"],
+                    value=value,
+                    external=external,
+                    complete=complete,
+                    hash=hash,
+                )
 
-            db.session.add(set_)
-            db.session.commit()
+                db.session.add(set_)
+                db.session.commit()
 
-            return jsonify(set_schema.dump(set_))
+                return jsonify(set_schema.dump(set_))
 
-        else:
-            return jsonify({"already": "exists"})
+            else:
+                return jsonify({"already": "exists"})
 
         else:
             return jsonify({"missing": "key or value"})
