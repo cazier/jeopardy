e552d092976bdb0c71f96922e5bce206b40df5ca
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-11-21 09:12:42 -0600

    Sqlalchemy uses a blank URI for in-memory

--- jeopardy_data/api/__init__.py
+++ jeopardy_data/api/__init__.py
@@ -5,7 +5,7 @@ from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = f":memory:"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app=app)
 ma = Marshmallow(app=app)
