fa48132fda5dd2c2bd43cbe79328b2920e098d65
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-11-21 09:14:33 -0600

    Updated test

--- tests/test_import.py
+++ tests/test_import.py
@@ -6,11 +6,15 @@ import jeopardy_data.api as api
 
 import_file = pathlib.Path("import_test.json").absolute()
 
-
 def test_delete_db():
     try:
         api.db.drop_all()
     except:
-        assert True, "database could not be deleted"
-        # api.db.create_all()
+        assert False, "database could not be deleted"
+
+def test_create_db():
+    try:
+        api.db.create_all()
+    except:
+        assert False, "database could not be deleted"
 
