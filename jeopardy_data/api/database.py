45183ce2e73bc3c33a87531e5618ce6c3a23bed6
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-11-21 15:27:31 -0600

    Tests and checks for clue adding

--- jeopardy_data/api/database.py
+++ jeopardy_data/api/database.py
@@ -4,6 +4,8 @@ import datetime
 from . import db
 from .models import *
 
+KEYS = {"date", "show", "round", "complete", "answer", "question", "external", "value", "category"}
+
 
 def add(clue_data: dict, uses_shortnames: bool = True) -> tuple:
     def key(key: str) -> str:
@@ -12,32 +14,78 @@ def add(clue_data: dict, uses_shortnames: bool = True) -> tuple:
 
         return clue_data[key]
 
-    if (date := Date.query.filter_by(date=datetime.date.fromisoformat(key("date"))).first()) is None:
-        date = Date(date=datetime.date.fromisoformat(key("date")))
+    keys = KEYS
+
+    if uses_shortnames:
+        keys = {i[0] if i != "complete" else "f" for i in keys}
+    
+    if (missing := keys.difference(set(clue_data.keys()))) != set():
+        return False, {"message": f"this set is missing the following keys: {' '.join(missing)}"}
+
+    for k, v in clue_data.items():
+        if len(str(v)) == 0:
+            return False, {"message": f"this set has an empty value for key: {k}"}
+
+    try:
+        date_format = datetime.date.fromisoformat(key("date"))
+    
+    except ValueError:
+        return False, {"message": "please format the date in the isoformat: YYY-MM-DD"}
+
+    if (date := Date.query.filter_by(date=date_format).first()) is None:
+        date = Date(date=date_format)
         db.session.add(date)
 
-    if (show := Show.query.filter_by(number=key("show")).first()) is None:
-        show = Show(number=key("show"), date=date)
+    try:
+        show_format = int(key("show"))
+    
+    except ValueError:
+        return False, {"message": "please ensure the show number is an integer (positive or negative)"}
+
+    if (show := Show.query.filter_by(number=show_format).first()) is None:
+        show = Show(number=show_format, date=date)
         db.session.add(show)
 
-    if (round_ := Round.query.filter_by(number=key("round")).first()) is None:
-        round_ = Round(number=key("round"))
+    try:
+        round_format = int(key("round"))
+        if round_format not in (0, 1, 2, 4):
+            raise ValueError
+    
+    except ValueError:
+        return False, {"message": "please ensure the round number is one of the following integers: (0, 1, 2, 4)"}
+
+    if (round_ := Round.query.filter_by(number=round_format).first()) is None:
+        round_ = Round(number=round_format)
         db.session.add(round_)
 
-    if (complete := Complete.query.filter_by(state=key("complete")).first()) is None:
-        complete = Complete(state=key("complete"))
+    if (complete_format := key("complete")) not in (True, False):
+        return False, {"message": "please ensure the complete tag is supplied with a boolean value"}
+
+    if (complete := Complete.query.filter_by(state=complete_format).first()) is None:
+        complete = Complete(state=complete_format)
         db.session.add(complete)
 
     if (category := Category.query.filter_by(name=key("category")).filter_by(date=date).first()) is None:
         category = Category(name=key("category"), show=show, round=round_, complete=complete, date=date)
         db.session.add(category)
 
-    if (value := Value.query.filter_by(amount=key("value")).first()) is None:
-        value = Value(amount=key("value"), round=round_)
+    try:
+        value_format = int(str(key("value")).replace("$", ""))
+        if value_format < 0:
+            raise ValueError
+    
+    except ValueError:
+        return False, {"message": "please ensure the value is a positive number, with or without, a \"$\""}
+
+    if (value := Value.query.filter_by(amount=value_format).first()) is None:
+        value = Value(amount=value_format, round=round_)
         db.session.add(value)
 
-    if (external := External.query.filter_by(state=key("external")).first()) is None:
-        external = External(state=key("external"))
+    if (external_format := key("external")) not in (True, False):
+        return False, {"message": "please ensure the external tag is supplied with a boolean value"}
+
+    if (external := External.query.filter_by(state=external_format).first()) is None:
+        external = External(state=external_format)
         db.session.add(external)
 
     hash = zlib.adler32(f'{key("question")}{key("answer")}{show.number}'.encode())
@@ -62,5 +110,5 @@ def add(clue_data: dict, uses_shortnames: bool = True) -> tuple:
         return True, set_
 
     else:
-        return False, {"already": "exists"}
+        return False, {"message": "this set already exists"}
 
--- tests/test_import.py
+++ tests/test_import.py
@@ -1,4 +1,5 @@
 import pathlib
+import json
 
 import pytest
 
@@ -17,4 +18,262 @@ def test_create_db():
         api.db.create_all()
     except:
         assert False, "database could not be deleted"
+
+def test_add_one_long():
+    clue = {
+        "date": "2020-01-01",
+        "show": 1,
+        "round": 0,
+        "complete": True,
+        "answer": "answer",
+        "question": "question",
+        "external": True,
+        "value": 1,
+        "category": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=False)
+    assert success, message
+
+def test_add_one_long_missing():
+    clue = {
+        "date": "2020-01-01",
+        "show": 1,
+        "round": 0,
+        "answer": "answer",
+        "question": "question",
+        "external": True,
+        "value": 1,
+        "category": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=False)
+    assert not success, message
+
+def test_add_one_short():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert success, message
+
+def test_add_one_short_missing():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert not success, message
+
+def test_add_multiple_short():
+    clues = [{
+        "d": "2020-01-01",
+        "s": 3,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    },
+    {
+        "d": "2020-01-01",
+        "s": 4,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }]
+    results = [api.database.add(clue_data=clue, uses_shortnames=True) for clue in clues]
+    
+    assert all((response[0] for response in results))
+
+def test_add_one_empty():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+
+    results = list()
+
+    for key in clue.keys():
+        data = {k: v if k != key else "" for k, v in clue.items()}
+        results.append(api.database.add(clue_data= data, uses_shortnames=True))
+
+    assert all((not response[0] for response in results))
+
+def test_add_one_repeat():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "this set already exists"}))
+
+
+def test_add_one_repeat():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "this set already exists"}))
+
+def test_add_bad_date():
+    clue = {
+        "d": "20210-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please format the date in the isoformat: YYY-MM-DD"}))
+
+def test_add_bad_show():
+    clue = {
+        "d": "2020-01-01",
+        "s": "alex",
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please format the date in the isoformat: YYY-MM-DD"}))
+
+def test_add_bad_round_not_integer():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": "alex",
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please ensure the round number is one of the following integers: (0, 1, 2, 4)"}))
+
+
+def test_add_bad_round_not_valid():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 3,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please ensure the round number is one of the following integers: (0, 1, 2, 4)"}))
+
+def test_add_bad_complete():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": "alex",
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please ensure the complete tag is supplied with a boolean value"}))
+
+def test_add_bad_value_not_integer():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": "alex",
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please ensure the value is a positive number, with or without, a \"$\""}))
+
+def test_add_bad_value_not_positive():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": True,
+        "v": -1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please ensure the value is a positive number, with or without, a \"$\""}))
+
+def test_add_bad_external():
+    clue = {
+        "d": "2020-01-01",
+        "s": 2,
+        "r": 0,
+        "f": True,
+        "a": "answer",
+        "q": "question",
+        "e": "alex",
+        "v": 1,
+        "c": "test"
+    }
+    success, message = api.database.add(clue_data=clue, uses_shortnames=True)
+    assert (not success & (message == {"message": "please ensure the external tag is supplied with a boolean value"}))
 
