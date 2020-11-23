3d7f307ab22a5a4cd198712ce5135bf631628426
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-11-22 18:01:09 -0600

    Starting scraper testing

---
+++ tests/test_scraping.py
@@ -0,0 +1,72 @@
+import pathlib
+import json
+import os
+
+import pytest
+import requests
+
+import jeopardy_data.scraping.scrape as scrape
+
+
+@pytest.fixture
+def PatchedRequests(monkeypatch):
+    standard = requests.get
+
+    def localGet(uri, *args, **kwargs):
+        import time
+        file = uri.split("/")[-1]
+
+        path = pathlib.Path(os.getcwd(), "tests/files/mock_get", file).absolute()
+        
+        if path.exists():
+            status_code = 200
+            
+            with open(path, "r") as local_file:
+                text = local_file.read()
+        
+        else:
+            status_code = 404
+
+            text = "<html><head></head><body>404 Page Not Found</body></html>"
+        
+        mock = type("PatchedRequests", (), {})()
+        mock.status_code = status_code
+        mock.text = text
+
+        return mock
+    
+    monkeypatch.setattr(requests, 'get', localGet)
+
+@pytest.fixture
+def example_org(PatchedRequests):
+    scrape.BASE_URL = "https://example.org"
+    scrape.DELAY = 0
+
+    return scrape.Webpage(resource="test")
+
+def test_webpage_url_creation(example_org):
+    assert example_org.url == "https://example.org/test"
+
+def test_webpage_repr(example_org):
+    assert str(example_org) == "https://example.org/test"
+
+def test_storage(example_org):
+    assert example_org.storage == pathlib.Path("", "test").absolute()
+
+def test_get_download(example_org):
+    success, bs = example_org.get()
+    assert success & (bs.body.text == '"Page Definitely Absolutely Found"')
+
+def test_caching(example_org):
+    scrape.CACHE = True
+    scrape.CACHE_PATH = pathlib.Path(os.getcwd(), "tests/files/cache").absolute()
+
+    assert not pathlib.Path(os.getcwd(), "tests/files/cache/test").exists
+    example_org.get()
+    assert pathlib.Path(os.getcwd(), "tests/files/cache/test").exists
+
+def test_get_download_404():
+    page = scrape.Webpage(resource="404")
+
+    success, message = page.get()
+    assert not success & (message == {"message": "failed to receive webpage data"})
