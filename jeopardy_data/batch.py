aec7ee0a72600af360fd66929dbb89de928746b4
Brendan Cazier <520246+cazier@users.noreply.github.com>
2020-11-25 14:25:57 -0600

    Moved (current) scraping code to batch file

--- jeopardy_data/batch.py
+++ jeopardy_data/batch.py
@@ -89,5 +89,116 @@ def add(filename: str, method: str, progress: bool, url: str = "", shortnames: b
                 raise NotImplementedError
 
 
+# class Pull(object):
+#     def __init__(
+#         self,
+#         start: int = 1,
+#         stop: int = 36,
+#         include_special: bool = False,
+#         initial: bool = False,
+#         error_only: bool = False,
+#         shortnames: bool = False,
+#         method: str = "db",
+#     ):
+#         self.start = start
+#         self.stop = stop
+#         self.include_special = include_special
+#         self.method = method
+#         self.shortnames = shortnames
+#         self.json_backup = list()
+
+#         print("Pulling season data")
+#         start = time.perf_counter()
+#         if error_only:
+#             with open("status.json", "r") as json_file:
+#                 json_data = json.load(json_file)
+
+#             self.error = list()
+#             self.success = json_data["success"]
+#             self.pending = json_data["pending"] + json_data["error"]
+#             self.outstanding_clues = json_data["out_clues"]
+
+#             self.save(clues=False)
+#             initial = False
+
+#         if initial:
+#             seasons = get_seasons(start=self.start, stop=self.stop, include_special=self.include_special)
+#             store_initial_games(seasons)
+
+#         elapsed = time.perf_counter() - start
+#         print(f"Finished pulling season data! Time taken: {elapsed:.2f} seconds")
+
+#         if input("Saved all season data to JSON file. Continue? (yN)").lower() == "y":
+#             with open("status.json", "r") as json_file:
+#                 json_data = json.load(json_file)
+
+#             self.error = json_data["error"]
+#             self.success = json_data["success"]
+#             self.pending = json_data["pending"]
+
+#             self.outstanding_clues = json_data["out_clues"]
+
+#             self.scrape()
+
+#     def save(self, clues: bool = True):
+#         with open("status.json", "w") as json_file:
+#             json.dump(
+#                 {
+#                     "error": self.error,
+#                     "success": self.success,
+#                     "pending": self.pending,
+#                     "out_clues": self.outstanding_clues,
+#                 },
+#                 json_file,
+#                 indent="\t",
+#             )
+
+#         if clues:
+#             with open("clue_backup.json", "w") as json_file:
+#                 json.dump(self.json_backup, json_file, indent="\t")
+
+#     def scrape(self):
+#         print("Starting to scrape game data")
+#         while len(self.pending) > 0:
+#             start = time.perf_counter()
+#             url = self.pending.pop()
+
+#             try:
+#                 if url not in self.error and url not in self.success:
+#                     clues = Game(url=url)
+
+#                     self.outstanding_clues = clues.json
+
+#                     while len(self.outstanding_clues) > 0:
+#                         clue = self.outstanding_clues.pop()
+
+#                         if self.method == "db":
+#                             db_add(clue_data=clue, shortnames=self.shortnames)
+
+#                         elif self.method == "web":
+#                             web_add(url=self.url, clue_data=clue, shortnames=self.shortnames)
+
+#                         self.json_backup.append(clue)
+
+#             except KeyboardInterrupt:
+#                 self.save()
+#                 import sys
+
+#                 sys.quit()
+
+#             except:
+#                 print(f"An error occurred with game {clues.show}")
+#                 self.error.append(url)
+
+#             else:
+#                 elapsed = time.perf_counter() - start
+#                 print(f"Successfully scraped game {clues.show} Time taken: {elapsed:.2f} seconds")
+#                 self.success.append(url)
+
+#                 time.sleep(0.5)
+
+#         self.save()
+
+
 # if __name__ == "__main__":
 #     add(filename="../json/by_year/*.json", method="db")
