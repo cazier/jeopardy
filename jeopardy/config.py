8b17ca9b3da2e319e2c1174d8ba3e846ad9dbe9d
Brendan Cazier <520246+cazier@users.noreply.github.com>
2021-01-19 13:44:22 -0600

    Updated relative API endpoint

--- jeopardy/config.py
+++ jeopardy/config.py
@@ -1,6 +1,7 @@
 import os
 import uuid
 
+game_version = 1.0
 api_version = 1
 
 game_name = "Jeopardy"
@@ -9,11 +10,12 @@ currency = "$"
 secret_key = os.getenv(key="SECRET_KEY", default="correctbatteryhorsestaple")
 
 
-debug = False
+debug = True
 sets = 2
 start_round = 0
 
-api_endpoint = "http://127.0.0.1:5001/game"
+api_endpoint = f"http://127.0.0.1:5000/api/v{api_version}/game"
+api_db = os.getenv(key="DB_FILE", default="sample.db")
 
 access_key = uuid.uuid4().hex
 
