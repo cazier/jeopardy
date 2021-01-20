c5b94d23fae3bd10541929cc27a35887015baf90
Brendan Cazier <520246+cazier@users.noreply.github.com>
2021-01-20 09:19:42 -0600

    Fix for import error in pytest

--- jeopardy/sockets.py
+++ jeopardy/sockets.py
@@ -1,7 +1,11 @@
 from flask_socketio import SocketIO, join_room, rooms
 from flask import request
 
-import config
+try:
+    import config
+
+except ImportError:
+    from jeopardy import config
 
 socketio = SocketIO(cors_allowed_origins=config.url)
 
