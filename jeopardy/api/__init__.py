29b71c028130e1dcee45a60bff40660fb805cb7a
Brendan Cazier <520246+cazier@users.noreply.github.com>
2023-08-26 22:12:03 +0000

    fixup! Custom serializing

--- jeopardy/api/__init__.py
+++ jeopardy/api/__init__.py
@@ -2,11 +2,7 @@ from flask import Blueprint
 
 from jeopardy import config
 
-# from flask_marshmallow import Marshmallow
-
-
 bp = Blueprint(name="api", import_name=__name__, url_prefix=f"/api/v{config.api_version}")
-# ma = Marshmallow(app=bp)
 
 KEYS = {"date", "show", "round", "complete", "category", "value", "external", "question", "answer"}
 
